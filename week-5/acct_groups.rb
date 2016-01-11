369b00dcfa95c7c0aed29129ad79ceaa3decca2a
Emmet Susslin <esusslin@gmail.com>
2016-01-11 12:21:30 -0500

    weak

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,17 @@
+#Pseudocode
+# make an array of all of the fiddler crabs
+# count the amount of the crabs
+# divide that number so as not to leave a remainder of less than 3
+# 
+
+
+fiddler_crabs = ["Aaron Hu","Adam Pinsky","Akeem Street","Alex Forger","Andrew Kim","Baron Kwan",
+				"Brian Bier","Byron Gage","Carl Conroy","Charlie Bliss","Christopher Bunkers",
+				"Cody Kendall","Coline Forde","David Valencia","Emily Bosakowski","Everett Golden",
+				"Hagai Zwick","Heather C","Ian Wudarski","Ieronim Oltean","Jake Hamilton","James Boyd",
+				"Jasmeet Chatrath","Jenna El-Amin","Jerrie Evans","Joe Case","Jonathan Case",
+				"Jonathan Schwartz","Jonathan Silvestri","Kathryn Garbacz","Ian Kinner","Kyle Cierzan",
+				"Kyle Zelman","Linda Oanh Ho","Yiorgos Makridakis","Matt Harris","Matthew Baquerizo",
+				"Menuka Samaranayake","Michael Pintar","Mollie Stein","Lydia Nash","Aaron Opsahl","Peter Leong","
+				Peter Stratoudakis","Prince Sadie","Ryan F. Salerno","Sanderfer Chau","Sarah Finken","EMMET SUSSLIN",
+				"Sydney Rossman-Reich","Eric Tenza","Thomas Yancey","Tim Kelly","Timothy Beck"]
