523ab5e340011f3445b8f48495a8e0508a38f09c
Emmet Susslin <esusslin@gmail.com>
2016-01-18 13:18:43 -0500

    yes

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -1,3 +1,6 @@
+
+# I spent 2 hours on this challenge.with sarab
+
 # RELEASE 2: NESTED STRUCTURE GOLF
 # Hole 1
 # Target element: "FORE"
@@ -5,7 +8,12 @@
 array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 
 # attempts:
-# ============================================================
+# p array[1][1][3][0]
+p array[1][1][2][0]
+
+
+
+#============================================================
 
 
 
@@ -17,7 +25,8 @@ array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 
 # attempts:
-# ============================================================
+p hash [:outer][:inner]["almost"][3]
+#nested_hash = {:outer_key => {:inner_key => "Winner!"}} ============================================================
 
 
 
@@ -30,6 +39,7 @@ hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 nested_data = {array: ["array", {hash: "finished"}]}
 
 # attempts:
+p nested_data [:array][1][:hash]
 # ============================================================
 
 
@@ -40,8 +50,45 @@ nested_data = {array: ["array", {hash: "finished"}]}
 
 number_array = [5, [10, 15], [20,25,30], 35]
 
+def add_five(number_array)
+
+  number_array.each do |element|
+
+    #if element.is_a? (Integer)
+     # number_array.map! {|y| y + 5}
+
+    if element.kind_of?(Array)
+      element.map! {|x| x + 5}
+
+    else
+      element.kind_of?(Integer)
+      number_array.map! {|i| i + 5}
+      end
+    end
+
+  end
+
+#We couldn't quite get this one to work.
 
 
+p add_five(number_array)
+
+
+# The nested_array.each iterates over all elements, then the element.each iterates over inner arrays if there are any.
 # Bonus:
 
-startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+#startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+#Reflection
+=begin
+
+What are some general rules you can apply to nested arrays?
+* all of the outer layers of the array need to be addressed to access or call inner values
+
+What are some ways you can iterate over nested arrays?
+* identifying the arrays with .each and then iterating over the inner arrays
+
+  end
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+We mainly used methods we already knew. We did try using .is_a? and .kind_of? which I handn't used before. I think these were the right kind of methods to use in order to treat the arrays and integers in Release 3 differently. However, we couldn't quite get the syntax or something to work right.
+=end
