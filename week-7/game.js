1857beb8e860bc2f27a13b95de1dc51d9d96c9d3
Emmet Susslin <esusslin@gmail.com>
2016-01-25 09:56:17 -0500

    acct

---
+++ week-7/game.js
@@ -0,0 +1,64 @@
+ // Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission: to create a casino with multiple games and players
+// Goals: to create multiple players 		
+// Characters: DiamondJim, MarcoVegas, 
+// Objects: 
+//    *
+// Functions: slot machine: rotates 3 arrays of various numbers
+				* accepts bets
+				* based on matches, returns money
+
+// Pseudocode
+// 3 cowboy players are established each with their own purse
+// 
+//
+//
+//
+
+// Initial Code
+//
+//
+
+var diamondJim {
+	purse: 47.53;
+	age: 47;
+	heat: "Colt";
+	bet: 15;
+}
+
+var virgil {
+	purse: 76.89;
+	age: 38;
+	heat: "Lonestar";
+	bet: 10;
+}
+
+var marcoVegas {
+	purse: 89.27;
+	age: 33;
+	heat: "Remington";
+	bet: 20;
+}
+
+console.log(marcoVegas.purse)
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,93 @@
+ // JavaScript Olympics
+
+// I paired [by myself, with:] on this challenge.
+
+// This challenge took me [#] hours.
+
+
+// Warm Up
+
+// Bulk Up
+var athlete1 = ["Helga","knife throw"];
+var athlete2 = ["Norman","javeline toss"];
+var athlete3 = ["Virgil","quick draw"];
+
+var input = [athlete1, athlete2, athlete3];
+
+var win = function(array) {
+  for (var counter = 0; counter < array.length; counter++)
+    array[counter].push("won the");
+  for (var counter = 0; counter < array.length; counter++)
+    console.log(array[counter][0] + " " + array[counter][2] + " " + array[counter][1] + "!");
+}
+
+
+var win_all_we_need = function(array) {
+  for (var counter = 0; counter < array.length; counter++)
+    console.log(array[counter][0] + " won the " + array[counter][1] + "!");
+}
+win_all_we_need(input);
+
+// // console.log(input[0][0]);
+// function win(array)  {
+ 
+//   for (var counter = 0; counter<array.length; counter++) {
+  
+//   array[counter].push("won the");
+//     console.log(array[counter][0] + " " + array[counter][2] + " " + array[counter][1]);
+//   };
+  
+// }
+//   var second_athlete = {
+//      name: "Norman",
+//      event: "javeline toss",
+//  }; 
+//   var second_athlete = {
+//      name: "Virgil",
+//      event: "quick draw",
+//     };
+// };
+
+// win(input);
+
+
+// Jumble your words
+
+var word = "hello";
+
+var naiveReverse = function(string) {
+    return string.split('').reverse().join('');
+}
+console.log(naiveReverse(word));
+
+
+// 2,4,6,8
+
+var evens = function(array) {
+  var ending_array = [];
+  for (var counter = 0; counter < array.length; counter++)
+    if (array[counter] % 2 == 0)
+    ending_array.push(array[counter]);
+      // (array[counter]).push(ending_array);
+  return ending_array;
+}
+
+console.log(evens([1,2,3,4,5,6,7,8]));
+
+
+// "We built this city"
+function Athlete(name,age,sport,quote) {
+  this.name = name;
+  this.age = age;
+  this.sport = sport;
+  this.quote = quote;  
+};
+
+
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+
+
+
+// Reflection
